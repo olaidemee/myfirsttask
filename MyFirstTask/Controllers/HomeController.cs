b6b4baa5f2509ae90f44a4cd9ae0b1c70bc2ce27
olaide <laideawobayo@gmail.com>
2019-06-15 14:06:18 +0100

    commented out some codes

--- MyFirstTask/Controllers/HomeController.cs
+++ MyFirstTask/Controllers/HomeController.cs
@@ -34,10 +34,10 @@ namespace MyFirstTask.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
